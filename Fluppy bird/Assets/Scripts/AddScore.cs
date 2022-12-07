1261eab9cd3050c25a272dfb2ff42f0aae833309
Andry Saprigin <coffeei.2002@gamil.com>
2022-12-07 07:53:51 +0300

    commit

--- Fluppy bird/Assets/Scripts/AddScore.cs
+++ Fluppy bird/Assets/Scripts/AddScore.cs
@@ -2,6 +2,14 @@ using UnityEngine;
 
 public class AddScore : MonoBehaviour
 {
+    private AudioClip scoreClip;
+
+    private void Start()
+    {
+        scoreClip = GetComponent<AudioClip>();
+        Debug.Log(scoreClip);
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Bird"))
